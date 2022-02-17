a8b3ed8d62679f8a2dd65c8ba870ac37376697cb
vrodedanya <socurahlem@yandex.ru>
2022-02-17 22:07:44 +0300

    fix autotests

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,4 @@
 sudo apt-get install cmake libgtest-dev
 cd /usr/src/gtest
 sudo cmake CMakeLists.txt
-sudo make
-
-# copy or symlink libgtest.a and libgtest_main.a to your /usr/lib folder
-sudo cp lib/*.a /usr/lib
+sudo make install
