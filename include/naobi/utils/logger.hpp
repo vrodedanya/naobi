431146bac2ecd9db851d72cf29573740a15367cd
vrodedanya <socurahlem@yandex.ru>
2022-02-20 09:47:01 +0300

    Added logger

---
+++ include/naobi/utils/logger.hpp
@@ -0,0 +1,90 @@
+#ifndef NAOBI_LOGGER_HPP
+#define NAOBI_LOGGER_HPP
+
+#include <fstream>
+#include <iostream>
+#include <chrono>
+
+namespace naobi
+{
+	class logger
+	{
+	public:
+		logger() = delete;
+
+		template <typename... AArgs>
+		static void log(const std::string& addressee, int level, const AArgs&&... aargs);
+		static inline void setLoggingFile(const std::string& fileName);
+
+		static void enable(){_isEnabled = true;}
+		static void disable(){_isEnabled = false;}
+		static void enableLoggingToStdOut(){_useStdOut = true;}
+		static void disableLoggingToStdOut(){_useStdOut = false;}
+		static void setLevel(int level){_currentLevel = level;}
+	private:
+		template <typename T, typename... AArgs>
+		static void log_rec(const T&& type, const AArgs&&... aargs);
+
+		template <typename T>
+		static void log_rec(const T&& type);
+
+		static inline void print(const std::string& text);
+		static inline void println(const std::string& text);
+	private:
+		static inline std::string _currentAddressee;
+		static inline std::ofstream _file;
+		static inline bool _isEnabled{};
+		static inline bool _useStdOut{};
+		static inline int _currentLevel{};
+	};
+
+	void logger::setLoggingFile(const std::string &fileName)
+	{
+		if (_file.is_open()) _file.close();
+		_file.open(fileName, std::ios_base::out);
+	}
+
+	template <typename... AArgs>
+	void logger::log(const std::string &addressee, int level, const AArgs&&... aargs)
+	{
+		if (!_isEnabled) return;
+		if (level > _currentLevel) return;
+		std::time_t current_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+		if (addressee != _currentAddressee)
+		{
+			println("[" + addressee + "]:");
+		}
+		print("-> ");
+		print(std::ctime(&current_time));
+		print(" ")
+;		log_rec(std::forward<AArgs>(aargs)...);
+	}
+
+	template <typename T, typename... AArgs>
+	void logger::log_rec(const T &&type, const AArgs &&... aargs)
+	{
+		print(type);
+		print(" ");
+		log_rec(std::forward<AArgs>(aargs)...);
+	}
+
+	template <typename T>
+	void logger::log_rec(const T &&type)
+	{
+		println(type);
+	}
+
+	void logger::print(const std::string &text)
+	{
+		if (_useStdOut) std::cout << text;
+		if (_file.is_open()) _file << text;
+	}
+
+	void logger::println(const std::string &text)
+	{
+		if (_useStdOut) std::cout << text << std::endl;
+		if (_file.is_open()) _file << text << std::endl;
+	}
+}
+
+#endif //NAOBI_LOGGER_HPP
--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,15 @@
 #include <naobi/compiler/compiler.hpp>
+#include <naobi/utils/logger.hpp>
 
 int main(int argc, char* argv[])
 {
 	if (argc == 2)
 	{
+		naobi::logger::enable();
+		naobi::logger::enableLoggingToStdOut();
+		naobi::logger::setLevel(5);
+		naobi::logger::setLoggingFile("log.txt");
+
 		auto compositionOpt = naobi::compiler::compile(argv[1]);
 		if (compositionOpt.has_value())
 		{
