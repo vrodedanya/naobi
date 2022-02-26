b141a33aee328d34d514abb645bc30e22d537f54
vrodedanya <socurahlem@yandex.ru>
2022-02-26 15:50:30 +0300

    Fixed script for functional test

--- test/functional/functionalTester.py
+++ test/functional/functionalTester.py
@@ -1,12 +1,21 @@
 import os
 import subprocess
 
+
+def is_exist(path : str):
+    return os.path.isfile(path)
+
+
 if __name__ == "__main__":
     print("Begin functional tests")
     directories = next(os.walk("."))[1]
     for directory in directories:
         print("Run " + directory)
-        result = subprocess.run(["../../cmake-build-debug/./naobi", directory + "/main.naobi"])
+        if is_exist("../../cmake-build-debug/naobi"):
+            result = subprocess.run(["../../cmake-build-debug/./naobi", directory + "/main.naobi"])
+        else:
+            result = subprocess.run(["../../build/./naobi", directory + "/main.naobi"])
+
         if result.returncode != 0:
             print("Test " + directory + " failed!")
             exit(1)
