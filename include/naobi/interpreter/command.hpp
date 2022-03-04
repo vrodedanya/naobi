7f507fb0b08c3ab6535004b9b6713377ecf8dbdd
vrodedanya <socurahlem@yandex.ru>
2022-03-04 22:38:11 +0300

    Added command

---
+++ include/naobi/interpreter/command.hpp
@@ -0,0 +1,27 @@
+#ifndef NAOBI_COMMAND_HPP
+#define NAOBI_COMMAND_HPP
+
+namespace naobi
+{
+	struct workflow_context;
+
+	struct command
+	{
+		using argumentsList = std::vector<std::string>;
+		using implementation = std::function<void(std::shared_ptr<workflow_context> ptr, const argumentsList& arguments)>;
+		enum class names
+		{
+		// CommandName <-> argumentsCount
+			NEW, // 2 - name, type
+			ADD, // 0
+			LOAD, // 1 - number
+			SAVE, // 1 - number
+		};
+
+		names name;
+		implementation impl;
+		argumentsList arguments;
+	};
+}
+
+#endif //NAOBI_COMMAND_HPP
