e432978c224160a13251843b64e12984fa50e66c
vrodedanya <socurahlem@yandex.ru>
2022-03-04 22:38:32 +0300

    Update variable class

--- include/naobi/data/variable.hpp
+++ include/naobi/data/variable.hpp
@@ -4,22 +4,38 @@
 #include <string>
 #include <utility>
 #include <memory>
+#include <variant>
 
 namespace naobi
 {
 	class variable
 	{
 	public:
+		using variable_type = std::variant<int>;
 		using uptr = std::unique_ptr<variable>;
 		using sptr = std::shared_ptr<variable>;
+
+		enum class Type
+		{
+			INTEGER,
+		};
 	public:
-		explicit variable(std::string name);
+		variable(std::string name, Type type);
 
 		[[nodiscard]] std::string name() const {return _name;}
 
+		[[nodiscard]] Type type() const {return _type;}
+
+		variable_type& value(){return _value;}
+
 	private:
 		std::string _name;
+		variable_type _value{};
+		Type _type{Type::INTEGER};
 	};
+
+	naobi::variable::sptr operator += (const naobi::variable::sptr& variable1, const naobi::variable::sptr& variable2);
+
 }
 
 #endif //NAOBI_VARIABLE_HPP
--- source/data/variable.cpp
+++ source/data/variable.cpp
@@ -1,7 +1,21 @@
+#include <utility>
 #include <naobi/data/variable.hpp>
 
-naobi::variable::variable(std::string name) :
-	_name(std::move(name))
+naobi::variable::variable(std::string name, naobi::variable::Type type) :
+	_name(std::move(name)),
+	_type(type)
 {
 
+}
+
+naobi::variable::sptr naobi::operator+=(const naobi::variable::sptr& variable1, const naobi::variable::sptr& variable2)
+{
+	if (variable1->type() == variable::Type::INTEGER && variable1->type() == variable2->type())
+	{
+		int a = std::get<int>(variable1->value()) + std::get<int>(variable2->value());
+		auto newVariable = std::make_shared<naobi::variable>("temp", variable::Type::INTEGER);
+		newVariable->value() = a;
+		return newVariable;
+	}
+	return nullptr;
 }
