2a82ce49e24554453871c6f51767123792306990
vrodedanya <socurahlem@yandex.ru>
2022-03-08 22:57:02 +0300

    Fixed tabulation

--- source/compiler/code_generator.cpp
+++ source/compiler/code_generator.cpp
@@ -53,78 +53,79 @@ naobi::code_generator::createCommand(naobi::command::names name, const naobi::co
 
 
 std::map<naobi::command::names, naobi::command::implementation> naobi::code_generator::_commands =
-		{
-			{naobi::command::names::ADD,
-			[](const workflow_context::sptr& context, [[maybe_unused]]const command::argumentsList& arguments){
-				auto top = context->stack.top();
-				context->stack.pop();
-				context->stack.top() += top;
-			}},
-			{naobi::command::names::NEW,
-			[](const workflow_context::sptr& context, [[maybe_unused]]const command::argumentsList& arguments){
-				auto type = static_cast<naobi::variable::Type>(std::stoi(arguments[1]));
-				context->variables.push_back(std::make_shared<naobi::variable>(arguments[0], type));
-			}},
-			{naobi::command::names::LOAD, // TODO Difficult moment: variable can be load as reference or as copy
-			[](const workflow_context::sptr& context, [[maybe_unused]]const command::argumentsList& arguments){ //
-				context->stack.push(context->variables[std::stoul(arguments[0])]);
-			}},
-			{naobi::command::names::SAVE,
-			[](const workflow_context::sptr& context, [[maybe_unused]]const command::argumentsList& arguments){
-				context->variables[std::stoul(arguments[0])] = context->stack.top();
-				context->stack.pop();
-			}},
-			{naobi::command::names::PLACE,
-			[](const workflow_context::sptr& context, [[maybe_unused]]const command::argumentsList& arguments){
-				auto type = static_cast<naobi::variable::Type>(std::stoi(arguments[0]));
-				auto var = std::make_shared<naobi::variable>("temp", type);
-				if (type == naobi::variable::Type::INTEGER) var->value() = std::stoi(arguments[1]);
-				context->stack.push(var);
-			}},
-			{naobi::command::names::PRINTLN,
-			[](const workflow_context::sptr& context, [[maybe_unused]]const command::argumentsList& arguments){
-				auto val = context->stack.top();
-				std::cout << *val << std::endl;
-			}},
-			{naobi::command::names::PRINT,
-			[](const workflow_context::sptr& context, [[maybe_unused]]const command::argumentsList& arguments){
-				auto val = context->stack.top();
-				std::cout << *val;
-			}},
-		};
+{
+	{naobi::command::names::ADD,
+	[](const workflow_context::sptr& context, [[maybe_unused]]const command::argumentsList& arguments){
+		auto top = context->stack.top();
+		context->stack.pop();
+		context->stack.top() += top;
+	}},
+	{naobi::command::names::NEW,
+	[](const workflow_context::sptr& context, [[maybe_unused]]const command::argumentsList& arguments){
+		auto type = static_cast<naobi::variable::Type>(std::stoi(arguments[1]));
+		context->variables.push_back(std::make_shared<naobi::variable>(arguments[0], type));
+	}},
+	{naobi::command::names::LOAD, // TODO Difficult moment: variable can be load as reference or as copy
+	[](const workflow_context::sptr& context, [[maybe_unused]]const command::argumentsList& arguments){ //
+		context->stack.push(context->variables[std::stoul(arguments[0])]);
+	}},
+	{naobi::command::names::SAVE,
+	[](const workflow_context::sptr& context, [[maybe_unused]]const command::argumentsList& arguments){
+		context->variables[std::stoul(arguments[0])] = context->stack.top();
+		context->stack.pop();
+	}},
+	{naobi::command::names::PLACE,
+	[](const workflow_context::sptr& context, [[maybe_unused]]const command::argumentsList& arguments){
+		auto type = static_cast<naobi::variable::Type>(std::stoi(arguments[0]));
+		auto var = std::make_shared<naobi::variable>("temp", type);
+		if (type == naobi::variable::Type::INTEGER) var->value() = std::stoi(arguments[1]);
+		context->stack.push(var);
+	}},
+	{naobi::command::names::PRINTLN,
+	[](const workflow_context::sptr& context, [[maybe_unused]]const command::argumentsList& arguments){
+		auto val = context->stack.top();
+		std::cout << *val << std::endl;
+	}},
+	{naobi::command::names::PRINT,
+	[](const workflow_context::sptr& context, [[maybe_unused]]const command::argumentsList& arguments){
+		auto val = context->stack.top();
+		std::cout << *val;
+	}},
+};
 
-std::vector<naobi::code_generator::generatorRule> naobi::code_generator::_generatorRules = {
-		{[](const std::vector<std::string>& words) -> bool{ // Create variable
-			return naobi::keywords::checkIsType(words[0]);
-		},[](const std::vector<std::string>& words, std::vector<naobi::command>& commands){
-			naobi::variable::Type type;
-			if (words[0] == "integer") type = naobi::variable::Type::INTEGER;
-			else type = naobi::variable::Type::UNDEFINED;
-			commands.emplace_back(
-					naobi::code_generator::createCommand(
-					naobi::command::names::NEW,{words[1], std::to_string(static_cast<int>(type))}));
-		}},
-		{[](const std::vector<std::string>& words) -> bool{ // Create assignment
-			return words[1] == "=";
-		},[](const std::vector<std::string>& words, std::vector<naobi::command>& commands){
-			// todo expression handler
-			naobi::variable::Type type = naobi::variable::Type::INTEGER;
+std::vector<naobi::code_generator::generatorRule> naobi::code_generator::_generatorRules =
+{
+	{[](const std::vector<std::string>& words) -> bool{ // Create variable
+		return naobi::keywords::checkIsType(words[0]);
+	},[](const std::vector<std::string>& words, std::vector<naobi::command>& commands){
+		naobi::variable::Type type;
+		if (words[0] == "integer") type = naobi::variable::Type::INTEGER;
+		else type = naobi::variable::Type::UNDEFINED;
+		commands.emplace_back(
+				naobi::code_generator::createCommand(
+				naobi::command::names::NEW,{words[1], std::to_string(static_cast<int>(type))}));
+	}},
+	{[](const std::vector<std::string>& words) -> bool{ // Create assignment
+		return words[1] == "=";
+	},[](const std::vector<std::string>& words, std::vector<naobi::command>& commands){
+		// todo expression handler
+		naobi::variable::Type type = naobi::variable::Type::INTEGER;
 
-			commands.emplace_back(
-					naobi::code_generator::createCommand(
-							naobi::command::names::PLACE, {std::to_string(static_cast<int>(type)), words[2]}));
-			commands.emplace_back(
-					naobi::code_generator::createCommand(
-								naobi::command::names::SAVE, {"0"}));
-		}},
-		{[](const std::vector<std::string>& words) -> bool{ // Create printing
-			return words[0] == "println";
-		},[]([[maybe_unused]]const std::vector<std::string>& words, std::vector<naobi::command>& commands){
-			commands.emplace_back(
-					naobi::code_generator::createCommand(
-							naobi::command::names::LOAD, {"0"}));
-			commands.emplace_back(
-					naobi::code_generator::createCommand(
-							naobi::command::names::PRINTLN, {}));
-		}},
+		commands.emplace_back(
+				naobi::code_generator::createCommand(
+						naobi::command::names::PLACE, {std::to_string(static_cast<int>(type)), words[2]}));
+		commands.emplace_back(
+				naobi::code_generator::createCommand(
+							naobi::command::names::SAVE, {"0"}));
+	}},
+	{[](const std::vector<std::string>& words) -> bool{ // Create printing
+		return words[0] == "println";
+	},[]([[maybe_unused]]const std::vector<std::string>& words, std::vector<naobi::command>& commands){
+		commands.emplace_back(
+				naobi::code_generator::createCommand(
+						naobi::command::names::LOAD, {"0"}));
+		commands.emplace_back(
+				naobi::code_generator::createCommand(
+						naobi::command::names::PRINTLN, {}));
+	}},
 };
