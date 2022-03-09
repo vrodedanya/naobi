92da765931c93f6248c6d484f3005fa3d5cce2e5
vrodedanya <socurahlem@yandex.ru>
2022-03-09 22:16:37 +0300

    Added new version of join function

--- include/naobi/utils/parser.hpp
+++ include/naobi/utils/parser.hpp
@@ -25,6 +25,9 @@ namespace naobi
 		static std::vector<std::string> split(const std::string& text, const std::vector<std::string>& splitters,
 											  const std::vector<std::string>& single, int mods = 0);
 		static std::string join(const std::vector<std::string>& strings, const std::string& delimiter) noexcept;
+		static std::string join(std::vector<std::string>::iterator& begin, std::vector<std::string>::iterator& end, const std::string& delimiter) noexcept;
+
+
 		static std::string removeExtraSpaces(const std::string& str) noexcept;
 		static std::string removeFirstSym(const std::string& str, char sym) noexcept;
 		static std::string removeSym(const std::string& str, char symbolToRemove) noexcept;
