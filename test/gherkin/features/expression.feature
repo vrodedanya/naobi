3acd6b84d425ca5d7e4d0474b75e84b9f2706f59
vrodedanya <socurahlem@yandex.ru>
2022-05-05 20:13:04 +0300

    fixing .gitignore

--- test/gherkin/features/expression.feature
+++ test/gherkin/features/expression.feature
@@ -1,13 +1,51 @@
-Feature: Arithmetic expression
-  Scenario: Sum
+Feature: Arithmetic and logical expressions
+  Scenario: Adding two positive numbers
     Given script:
       """
       import standard;
 
       workflow main
-      target begin
       {
         println(5+5);
       }
       """
     Then got 10
+  Scenario: Adding two positive numbers with spacers
+    Given script:
+      """
+      import standard;
+      workflow main
+      {
+      println(5 + 10)
+      }
+      """
+    Then got 15
+  Scenario Outline: Adding two positive numbers multiple cases
+    Given script:
+      """
+      import standard;
+      workflow main
+      {
+        println(input() + input());
+      }
+      """
+    When pass <number1>
+    When pass <number2>
+    Then got <result>
+  Examples:
+    | number1 | number2 | result |
+    | 5       | 10      | 15     |
+    | 156     | 244     | 400    |
+    | 5.0     | 4.3     | 9.3    |
+    | 100000  | 1000000 | 1100000|
+
+  Scenario: Subtracting two numbers
+    Given script:
+      """
+      import standard;
+      workflow main
+      {
+        println(10-5);
+      }
+      """
+    Then got 5
--- test/gherkin/radish/step.py
+++ test/gherkin/radish/step.py
@@ -1,27 +1,69 @@
 import os
 import subprocess
+import time
 
-from radish import given, then
+from radish import given, then, when
 
+
+# Support functions
 
 def is_exist(path: str):
     return os.path.isfile(path)
 
 
+def is_process_running(process):
+    return process.poll() is None
+
+
+def take_logs(byte_lines):
+    utf8_lines = []
+    for line in byte_lines:
+        utf8_lines.append(line.decode("UTF-8"))
+    return "".join(utf8_lines)
+
+
+# Steps
+
+
 @given("script:")
 def take_script(step):
-    args = ["--script", step.text]
+    args = ["--script", step.text, "--level", "5"]
     if is_exist("../../cmake-build-debug/naobi"):
         args.insert(0, "../../cmake-build-debug/./naobi")
     else:
         args.insert(0, "../../build/./naobi")
 
-    step.context.process = subprocess.Popen(args, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    step.context.process = subprocess.Popen(args, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+    # Waiting for compilation
+    time.sleep(0.1)
+
+
+@when("pass {number:d}")
+def take_input(step, number):
+    if not is_process_running(step.context.process):
+        raise ChildProcessError("Naobi interpreter is already finished with code: "
+                                f"${str(step.context.process.returncode)}\nLogs:\n"
+                                f"${take_logs(step.context.process.stderr.readlines())}")
+    step.context.process.stdin.write(bytes(str(number) + '\n', "UTF-8"))
+    step.context.process.stdin.flush()
 
 
 @then("got {number:d}")
 def take_result(step, number):
+    if not is_process_running(step.context.process):
+        if step.context.process.returncode != 0:
+            raise ChildProcessError(f"Naobi exit with code: "
+                                    f"${str(step.context.process.returncode)} \n Logs:\n"
+                                    f"${take_logs(step.context.process.stderr.readlines())}")
     out = step.context.process.stdout.readline().decode().replace('\n', '')
     integer = int(out)
     if integer != number:
-        raise AssertionError("Expected " + str(number) + " got " + str(integer))
+        raise AssertionError(f"Expected ${str(number)} got ${str(integer)}")
+
+
+@then("ends with the code {number:d}")
+def finish_with(step, number):
+    if is_process_running(step.context.process):
+        step.context.process.wait()
+    if step.context.process.returncode != number:
+        raise AssertionError(f"Expected ${number} got ${step.context.process.returncode}")
